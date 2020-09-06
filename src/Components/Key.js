236977def8cc2f931a3285a99f0fb13ab94a71e1
Trevor Luper <luper4@gmail.com>
2020-09-06 01:33:55 -0500

    changed Style

--- src/Components/Key.js
+++ src/Components/Key.js
@@ -4,9 +4,11 @@ import styles from './Key.module.css';
 const Key = ({name, letter, id}) =>{
 	return(
 		// ba bw3 pv5 ph5 grow ma2 b--black bg-black-40 white br3 f4
-			<div className={styles.key}>	
-				<h1 className=''>{letter}</h1>
-				<p className={styles.sound}>{name}</p>
+			<div className='grow'>
+				<div className={styles.key}>	
+					<h1 className=''>{letter}</h1>
+					<p className={styles.sound}>{name}</p>
+				</div>
 			</div>
 		)
 }
