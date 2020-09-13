46709c4acc3c8d1f49e24e8e7d737723b655f84c
Trevor Luper <luper4@gmail.com>
2020-09-13 01:25:36 -0500

    Added click functionality and changed styles

--- src/Components/Key.js
+++ src/Components/Key.js
@@ -3,9 +3,9 @@ import './Key.css';
 
 const Key =({letter, name, id, onClick}) =>{
 	return(
-			<div className='grow'>
-				<div data-key={id} className="key">	
-					<h1 className=''>{letter}</h1>
+			<div className='grow' >
+				<div data-key={id} className="key" onClick={onClick} >	
+					<h1>{letter}</h1>
 					<p className="sound">{name}</p>
 				</div>
 			</div>
--- src/Components/Keyboard.js
+++ src/Components/Keyboard.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import Key from './Key';
-import './Key.css';
-import clap from '../sounds/clap.wav';
-import hihat from '../sounds/hihat.wav';
-import kick from '../sounds/kick.wav';
-import openhat from '../sounds/openhat.wav';
-import boom from '../sounds/boom.wav';
-import ride from '../sounds/ride.wav';
-import snare from '../sounds/snare.wav';
-import tom from '../sounds/tom.wav';
-import tink from '../sounds/tink.wav';
+//Imports
+	import React from 'react';
+	import Key from './Key';
+	import './Key.css';
+	import clap from '../sounds/clap.wav';
+	import hihat from '../sounds/hihat.wav';
+	import kick from '../sounds/kick.wav';
+	import openhat from '../sounds/openhat.wav';
+	import boom from '../sounds/boom.wav';
+	import ride from '../sounds/ride.wav';
+	import snare from '../sounds/snare.wav';
+	import tom from '../sounds/tom.wav';
+	import tink from '../sounds/tink.wav';
 
+//Component Keyboard
 const Keyboard = ({beats}) => {
+	//Handles key press and plays audio
 	const handleKeyDown = (e) => {
 		//Selects audio and keys
 		const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-		 const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+		const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 		 // Checks if keyCode is equal to key ID and changes class to "playing" when key is pressed.
 		 beats.map ((user, i) =>{
 		 	if(e.keyCode === beats[i].id){
@@ -29,12 +32,26 @@ const Keyboard = ({beats}) => {
 		 //Plays audio
 		 audio.play();
 	}
-// Removes "playing" class after key is released
-	  function removeTransition(e) {
-	    if (e.propertyName !== 'transform') return;
-	    e.target.classList.remove('playing');
-	  }
-	  //React hook to change the DOM after render
+
+	//Handles click and plays audio
+	function handleClick (e) {
+		const x = e.currentTarget;
+		x.classList.add('playing');
+		let data = x.getAttribute('data-key');
+		const audio = document.querySelector(`audio[data-key="${data}"]`);
+		 if(!audio) return;
+		 audio.currentTime = 0;
+		 //Plays audio
+		 audio.play();
+	}
+
+	// Removes "playing" class after key is released
+	function removeTransition(e) {
+		  if (e.propertyName !== 'transform') return;
+		  e.target.classList.remove('playing');
+	}
+
+	//React hook to change the DOM after render
 	React.useEffect(() => {
 		// Listens for keypress
 		window.addEventListener('keydown', handleKeyDown);
@@ -43,34 +60,34 @@ const Keyboard = ({beats}) => {
 		//Loops through  array and and listens for 'transitionend' event and calls the removeTransition function
 		keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 	});
-		return (
-			<div className='flex flex-wrap flex-auto items-center justify-center '>
+
+//Render
+	return (
+		<div className='flex flex-wrap flex-auto items-center justify-center '>
+			<audio data-key="65"  src={clap}></audio>
+			<audio data-key="83" src={hihat}></audio>
+			<audio data-key="68" src={kick}></audio>
+			<audio data-key="70" src={openhat}></audio>
+			<audio data-key="71" src={boom}></audio>
+			<audio data-key="72" src={ride}></audio>
+			<audio data-key="74" src={snare}></audio>
+			<audio data-key="75" src={tom}></audio>
+			<audio data-key="76" src={tink}></audio>
 			{
-				// Adds audio files
+				//Loops through beats object and add a key for each one
+				 beats.map((user, i) => {
+					return (
+						<Key 
+							key={beats[i].id} 
+							letter={beats[i].key} 
+							name={beats[i].name} 
+							id={beats[i].id}
+							onClick={handleClick}
+						/>
+					); 
+				})
 			}
-				<audio data-key="65"  src={clap}></audio>
-				<audio data-key="83" src={hihat}></audio>
-				<audio data-key="68" src={kick}></audio>
-				<audio data-key="70" src={openhat}></audio>
-				<audio data-key="71" src={boom}></audio>
-				<audio data-key="72" src={ride}></audio>
-				<audio data-key="74" src={snare}></audio>
-				<audio data-key="75" src={tom}></audio>
-				<audio data-key="76" src={tink}></audio>
-				{
-					//Loops through beats object and add a key for each one
-					 beats.map((user, i) => {
-						return (
-							<Key 
-								key={beats[i].id} 
-								letter={beats[i].key} 
-								name={beats[i].name} 
-								id={beats[i].id}
-							/>
-						); 
-					})
-				}
-			</div>
-		)
+		</div>
+	)
 }
 export default Keyboard;
