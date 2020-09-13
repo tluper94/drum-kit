d8d4476c81fadec28ac3087af3327d7968b3721f
Trevor Luper <luper4@gmail.com>
2020-09-13 09:50:21 -0500

    Updates

---
+++ precache-manifest.37aa190d12cb8505e496a24f803c1113.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83b35aeac8424c1666748120befdb40",
+    "url": "/drum-kit/index.html"
+  },
+  {
+    "revision": "e9f8e9b44d86df200d6a",
+    "url": "/drum-kit/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a5c6346d2c33bd030697",
+    "url": "/drum-kit/static/css/main.3d0eb939.chunk.css"
+  },
+  {
+    "revision": "e9f8e9b44d86df200d6a",
+    "url": "/drum-kit/static/js/2.6948f98b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-kit/static/js/2.6948f98b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c6346d2c33bd030697",
+    "url": "/drum-kit/static/js/main.aa8eb243.chunk.js"
+  },
+  {
+    "revision": "49ac8c54f3cf64433f0e",
+    "url": "/drum-kit/static/js/runtime-main.8022c804.js"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/drum-kit/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/drum-kit/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/drum-kit/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/drum-kit/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/drum-kit/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/drum-kit/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/drum-kit/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/drum-kit/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/drum-kit/static/media/tom.572ed39d.wav"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77dadecb5e33a21f2a7340585cfe89ce.js"
+  "/drum-kit/precache-manifest.37aa190d12cb8505e496a24f803c1113.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/drum-kit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
